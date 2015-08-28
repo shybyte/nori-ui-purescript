57d0c1df892414a978f54d25b6bfb557345760a5
shybyte <ein@volloeko.de>
2015-08-28 17:41:00 +0200

    * Add Play Button * Translate to phoneme (fake) on button click * Display Phoneme

--- src/Main.purs
+++ src/Main.purs
@@ -14,29 +14,55 @@ import Halogen.Query.StateF (modify)
 import Halogen.Util (appendToBody)
 import qualified Halogen.HTML as H
 import qualified Halogen.HTML.Events as E
+import qualified Halogen.HTML.Events.Forms as E
+import qualified Halogen.HTML.Properties as P
 
 -- | The state of the application
-newtype State = State { on :: Boolean }
+newtype State = State
+  {
+    on :: Boolean,
+    lyrics :: String,
+    phonemes :: String
+  }
 
 initialState :: State
-initialState = State { on: false }
+initialState = State
+  {
+    on: false,
+    lyrics: "I love you",
+    phonemes: ""
+  }
 
 -- | Inputs to the state machine
-data Input a = ToggleState a
+data Input a =
+  ToggleState a |
+  Play a |
+  UpdateLyrics String a
 
 ui :: forall g p. (Functor g) => Component State Input g p
 ui = component render eval
   where
-
   render :: Render State Input p
   render (State state) = H.div_
     [ H.h1_ [ H.text "Toggle Button" ]
+    , H.textarea [P.value state.lyrics, E.onValueChange (E.input UpdateLyrics)]
+    , H.button [ E.onClick (E.input_ Play)]
+               [ H.text "Play"]
     , H.button [ E.onClick (E.input_ ToggleState) ]
                [ H.text (if state.on then "On" else "Off") ]
+    , H.p [P.class_ (H.className "phonemes")] [H.text state.phonemes]
     ]
 
   eval :: Eval Input State Input g
-  eval (ToggleState next) = modify (\(State state) -> State { on: not state.on }) $> next
+  eval (ToggleState next) = modify (\(State state) -> State (state { on = not state.on })) $> next
+  eval (UpdateLyrics textInput next) = modify (\(State state) -> State (state { lyrics = textInput })) $> next
+  eval (Play next) = modify (\(State state)
+    -> State (state
+      {
+        on = not state.on,
+        phonemes = state.lyrics ++ " Oh Yeahh!"
+      })) $> next
+
 
 main :: Eff (HalogenEffects ()) Unit
 main = runAff throwException (const (pure unit)) $ do
